f445ac35ec3acdb6892fd4bb8f187c3d876d06bc
Allison Lautz <allison.lautz@showpad.com>
2020-03-04 16:41:52 -0600

    updated to have buttons for desktop slider

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
 	?>
 
 
-	<section class="resources count-<?=count($resources);?>">
+	<section class="resources swipe count-<?=count($resources);?>">
 		<div class="wrapper">
 
 			<?php foreach($resources as $k => $v): ?>
@@ -55,24 +55,14 @@
 			<?php endforeach;?>
 
 		</div>
-	</section>
-
-
-	<section class="posts count-<?=count($posts);?>">
-		<div class="wrapper">
-			
-			<?php foreach($posts as $k => $v): ?>
-
-				<article>
-					<img src="https://api.fnkr.net/testimg/350x200/<?=$v;?>/FFF/?text=img+placeholder">
-					<h2>Lorem ipsum dolor sit amet</h2>
-					<p>consectetur adipiscing elit. Nunc rutrum leo at dui placerat, id tincidunt libero sollicitudin. Nulla ultricies eleifend porta. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Fusce venenatis diam odio, et auctor nibh aliquam eu. Proin ultrices molestie augue nec sollicitudin. Quisque vitae euismod justo. Ut viverra, ante eget efficitur sodales, purus ipsum sollicitudin dolor, non mollis est risus quis lectus. Duis sagittis pulvinar imperdiet.</p>
-				</article>
-
-			<?php endforeach;?>
-
+		<div class="buttons">
+			<div class="prev inactive"></div>
+			<div class="next"></div>
 		</div>
 	</section>
+
+
+
 
 
 
--- libs/touchSwipe.js
+++ libs/touchSwipe.js
@@ -1,119 +1,137 @@
-function touchSwipe(){
+
+/* this is meant to work w/desktop too (buttons on desktop) */
+
+function slider(){
+  var wrapper = document.querySelector('.swipe .wrapper');
+  var itemWidth = document.querySelector('.swipe article').offsetWidth;
+  var itemLength = document.querySelectorAll('.swipe article').length;
+
+  var show = Math.ceil(window.innerWidth / itemWidth);
 
 
-  var container = document.querySelectorAll('section');
-
-  var touchstartX = 0;
-  var touchendX = 0;
-
-  var cl;
-  var n;
+  var max = (itemLength - show);
 
 
-
-  var classes = [];
-
-  var arr = [];
-
-
-
-
-  if(container.length){
-    for(var i = 0; i < container.length; i++){
+  var pct = (100 / show).toFixed(2);
+  pct = parseFloat(pct);
 
 
 
-      container[i].addEventListener('touchstart', function (event) {
-        event.preventDefault();
 
-        n = this.querySelectorAll('article').length;
-        touchstartX = event.changedTouches[0].screenX;
-        cl = this.classList.value;
+  var next = document.querySelector('.swipe .next');
+  var prev = document.querySelector('.swipe .prev');
 
-
-        if(classes.indexOf(cl) == -1){
-          classes.push(cl)
-        }
-
-
-      }, false);
-
-
-      
-
-
-      container[i].addEventListener('touchend', function (event) {
-        event.preventDefault();
-
-        touchendX = event.changedTouches[0].screenX;
-
-        gesture(this, classes, cl);
-        
-
-      }, false);
-
-      
-
-    } // end for
+  var click = 0;
 
 
 
-    
-    function gesture(container, classes, cl){
-      var x = classes.indexOf(cl);
-      if (touchendX < touchstartX) {
-        arr[x] = typeof(arr[x]) == 'undefined' ? 1 : arr[x] + 1;
-        swipe('left', container, x)
-      }
-      else{
-        swipe('right', container, x)
-      }
 
+
+
+
+  /* mobile */
+
+  var touchstartX;
+  var touchendX;
+
+  wrapper.addEventListener('touchstart', function (event) {
+    event.preventDefault();
+
+    touchstartX = event.changedTouches[0].screenX;
+
+  }, false);
+
+
+
+
+
+
+  wrapper.addEventListener('touchend', function (event) {
+    event.preventDefault();
+
+    touchendX = event.changedTouches[0].screenX;
+
+    console.log(touchstartX);
+    console.log(touchendX);
+
+    if(touchstartX > touchendX){
+      console.log('next');
+      slideNext();
+    }
+    else if(touchstartX < touchendX){
+      console.log('prev');
+      slidePrev();
     }
 
 
-    function swipe(dir, container, x){
-
-      var slide = slide ? slide : arr[x] * 100; /* change the 100% to something else (such as 90%) depending on how much of the card is peeking out */
-
-      if(dir == 'left'){
-        if(arr[x] < n ){
-          container.querySelector('.wrapper').style.left = (slide * -1) + '%';
-        }
-        else{
-          arr[x] = 0;
-          container.querySelector('.wrapper').style.left = 0;
-        }
-      }
-      else if(dir == 'right'){
-
-        slide = (slide - 100)
-
-        if(arr[x] > 0 ){
-          container.querySelector('.wrapper').style.left = (slide * -1) + '%';
-          arr[x] = (arr[x]) - 1;
-        }
-        else{
-          arr[x] = 0;
-        }
-      }
+  }, false);
 
 
+
+
+
+  /* end mobile */
+
+
+  next.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    slideNext();
+
+  }, false);
+
+
+  prev.addEventListener('click', function (event) {
+    event.preventDefault();
+
+    slidePrev();
+
+
+
+  }, false);
+
+
+
+
+  function slideNext(){
+    prev.classList.contains('inactive') ? prev.classList.remove('inactive') : '';
+
+    if(click < max){
+      click += 1;
     }
 
+    else{
+      click = max;
+    }
+
+    wrapper.style.left = (-1 * click * pct) + '%';
+    (click == max ? next.classList.add('inactive') : '');
+  }
+
+
+
+  function slidePrev(){
+    next.classList.contains('inactive') ? next.classList.remove('inactive') : '';
+
+    if(click > 0){
+      click -= 1;
+    }
+    else if(click == 0){
+      prev.classList.add('inactive');
+    }
+
+    else{
+      click = 0;
+    }
+
+    wrapper.style.left = (-1 * click * pct) + '%';
+    (click == 0 ? prev.classList.add('inactive') : '');
+  }
 
 
 
 
 
-  } // end if
+}
 
-
-
-
-
-
-
-} // end function
-
-touchSwipe();
+slider();
